9c83420b129b55be59814b25daee6c8d93c41724
Threshika V S <115970544+Threshik@users.noreply.github.com>
2024-04-23 22:20:43 +0530

    Update RemoveDuplicatesInarray.java

--- RemoveDuplicatesInarray.java
+++ RemoveDuplicatesInarray.java
@@ -4,7 +4,7 @@ public static int removeDuplicate(int arr[])
   for(int i=1;i<n;i++){
     if(arr[i]!=arr[i-1]) //curent element compared with the previous element
     {
-      arr[count]=arr[i];
+      arr[count]=arr[i]; //checks only for the unique elements in the array and the duplicate element's first occurrence is only taken into account for only one time
       count++;
     }
     return count;
